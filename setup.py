059f762eab89d5ad149650db2dd694756b1099e6
Aaron Christianson <ninjaaron@gmail.com>
2016-08-04 05:23:15 -0500

    trying a crazy hack to get fastentrypoints working with pip

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
-import fastentrypoints
+try:
+    import fastentrypoints
+except ImportError:
+    from urllib import request
+    fastep = request.urlopen(
+        'https://raw.githubusercontent.com/ninjaaron/fast-entry_points/master/fastentrypoints.py'
+        )
+    namespace = {}
+    exec(fastep, namespace)
 
 setup(
     name='dirlog',
-    version='0.2.3',
+    version='0.2.4',
     description='keep a log of directories you visit to get back fast',
     long_description=open('README.rst').read(),
     url='https://github.com/ninjaaron/dirlog',
