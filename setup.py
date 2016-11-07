b561364ef9fb89534eda0a7a42abfde2254c4b62
Aaron Christianson <ninjaaron@gmail.com>
2016-11-07 12:37:40 +0100

    switch to the sane usage of fastentrypoints.

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from setuptools import setup
-
-try:
-    from urllib import request
-except ImportError:
-    import urllib2 as request
-
-fastep = request.urlopen('https://raw.githubusercontent.com/ninjaaron/fast-entry_points/master/fastentrypoints.py')
-namespace = {}
-exec(fastep.read(), namespace)
+import fastentrypoints
 
 setup(
     name='dirlog',
-    version='0.2.11',
+    version='1.0.0',
     description='keep a log of directories you visit to get back fast',
     long_description=open('README.rst').read(),
     url='https://github.com/ninjaaron/dirlog',
